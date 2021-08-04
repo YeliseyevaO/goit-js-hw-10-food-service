dcfc48a55d46d3a3b8c80ca3e7306985004edb10
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-08-04 14:23:38 +0300

    исправила ошибки

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
  import dishes from "./menu.json";
 import menuCardTpl from "./templates.hbs";
 
-
-const LIGHT = 'light-theme';
-const DARK = 'dark-theme';
+const Theme = {
+LIGHT: 'light-theme',
+DARK: 'dark-theme',
+};
 
 
 function createMenuMarkUp(dishes) {
@@ -20,11 +21,11 @@ const toggleEl = document.querySelector('.theme-switch__toggle');
 toggleEl.onchange = onToggleClick;
 
 function onPageLoad() {
-if (localStorage.getItem('theme')) {
+if (localStorage.getItem('theme') === Theme.DARK) {
     toggleEl.checked = true;
-    bodyEl.classList.add(DARK);
+    bodyEl.classList.add(localStorage.getItem('theme'));
 } else {
-    bodyEl.classList.add(LIGHT);
+    bodyEl.classList.add(Theme.LIGHT);
 }
 }
 
@@ -33,10 +34,10 @@ onPageLoad();
 function onToggleClick(evt) {
     
     if (toggleEl.checked === true) {
-        localStorage.setItem('theme', DARK);
-        bodyEl.classList.replace(LIGHT, DARK);
+        localStorage.setItem('theme', Theme.DARK);
+        bodyEl.classList.replace(Theme.LIGHT, Theme.DARK);
     } else {
-        localStorage.removeItem('theme');
-         bodyEl.classList.replace(DARK, LIGHT);
+        localStorage.setItem('theme', Theme.LIGHT);
+         bodyEl.classList.replace(Theme.DARK, Theme.LIGHT);
     }
 }
